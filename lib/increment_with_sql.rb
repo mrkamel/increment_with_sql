053f169e080051154990c7cfcd66bc199db38758
Benjamin Vetter <vetter@plainpicture.de>
2015-04-08 20:50:36 +0200

    Postgres fixes

--- lib/increment_with_sql.rb
+++ lib/increment_with_sql.rb
@@ -6,10 +6,8 @@ module IncrementWithSql
   def increment_with_sql!(attribute, by = 1)
     raise ArgumentError("Invalid attribute: #{attribute}") unless attribute_names.include?(attribute.to_s)
 
-    original_value_sql = "CASE WHEN `#{attribute}` IS NULL THEN 0 ELSE `#{attribute}` END"
-
     self.class.transaction do
-      self.class.where(:id => id).update_all("`#{attribute}` = #{original_value_sql} + #{by.to_i}")
+      self.class.where(:id => id).update_all("#{self.class.connection.quote_column_name attribute} = CASE WHEN #{self.class.connection.quote_column_name attribute} IS NULL THEN 0 ELSE #{self.class.connection.quote_column_name attribute} END + #{by.to_i}")
 
       send "#{attribute}=", self.class.where(:id => id).select(attribute).first.send(attribute)
 
